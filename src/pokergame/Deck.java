d02d7fe07480c5f7ce954739c69100ecc67f6bb1
jameswillby <jameswillby@192.168.1.71>
2013-02-09 14:45:05 +0000

    Version added 9 Feb 2013

--- src/pokergame/Deck.java
+++ src/pokergame/Deck.java
@@ -11,54 +11,53 @@ import java.math.*;
  */
 public class Deck {
     
-private static String[] suites = {"Clubs","Hearts","Spades","Diamonds"};    
+private static String[] suites = {"Clubs","Hearts","Spades","Diamonds"};  
+private static String[] names = {"Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten","Jack","Queen","King","Ace"};
+private static int[] values = {2,3,4,5,6,7,8,9,10,11,12,13,14};
 private Vector<Card> deck = null;
 
 public Deck()       
 {
-   deck = new Vector<Card>();
-    for (int x = 0; x < suites.length;x++)
-       {
-        Vector<Card> cards = new Suite(suites[x]).getCards();
-        for (int y = 0; y < cards.size();y++)
-        {        
-        deck.add(cards.get(y));
-        }
-       }
+        deck = new Vector<Card>(); // initialize the empty vector
+        for (int x = 0; x < suites.length;x++)
+        for(int y =0;y < names.length;y++)
+        deck.add(new Card(suites[x],names[y],values[y])); // add the cards of each suite in order
+       
 }
-
 public void printDeck()
 {
-for (int x=0; x < deck.size();x++)
-    System.out.println("The " + deck.elementAt(x).returnName()
-                        + " which has a value of " + deck.elementAt(x).getValue());
+        for (int x=0; x < deck.size();x++)
+        System.out.println("The " + deck.elementAt(x).returnName()
+        + " which has a value of " + deck.elementAt(x).getValue());
 }
-public int cardsIntheDeck(){return deck.size();}
+public int cardsIntheDeck()
+{
+        return deck.size();
+}
 
 public int generateRandomNumber(int size)
 
 {
- return (int)(Math.random()*size); // Standard pattern to generate a random number in
-}                                                 // the range that we want
+        return (int)(Math.random()*size); // Standard pattern to generate a random number
+}                                         
 
 
 public void shuffleTheCards()
 {
-if (deck.size() <= 1)
-    System.err.println("Cannot shuffle a deck of 0 or 1 cards");
-else
-    {
-             for(int x = 0;x < deck.size();x++) //Start at the second card
+    if (deck.size() <= 1)
+            System.err.println("Cannot shuffle a deck of 0 or 1 cards");
+    else
             {
-             int position1 = generateRandomNumber(deck.size()-1);
-             int position2 = generateRandomNumber(deck.size()-1);
-             Card card1 = deck.get(position1);
-             Card card2 = deck.get(position2);
-             deck.setElementAt(card2, position1);
-             deck.setElementAt(card1, position2);
+                for(int x = 0;x < deck.size();x++) //Start at the first card
+                {
+                     int position1 = generateRandomNumber(deck.size()-1);
+                     int position2 = generateRandomNumber(deck.size()-1);
+                     Card card1 = deck.get(position1);
+                     Card card2 = deck.get(position2);
+                     deck.setElementAt(card2, position1);
+                     deck.setElementAt(card1, position2);
+                }
             }
-    }
     
 }
-
 }
