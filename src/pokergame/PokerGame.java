eec6be0648bd120f0b5a1c9c6027de364b9e9b01
jameswillby <jameswillby@192.168.1.71>
2013-03-07 19:08:52 +0000

    Added in the Game class. Implemented the matchedCard method in Hand and overrode the equals method in Card. Created the GameTest class and tested for 5 cards and non-equal hands.

--- src/pokergame/PokerGame.java
+++ src/pokergame/PokerGame.java
@@ -19,34 +19,10 @@ public class PokerGame {
         // TODO code application logic here
         
         
-  runGame();
   
         
    
  }
-   
-    
-public static void runGame()
-{
-    /**
-        // initialise our players
-    
-        PokerGamePersonFactory personFactory = new PokerGamePersonFactory();
-        PokerGamePerson player,dealer; 
-        
-        player = personFactory.returnPerson("player");
-        dealer = personFactory.returnPerson("dealer");
-        
-        dealer.shuffleTheDeck(); // shuffles deck
-        
-        //deals 5 cards to player and dealer
-        
-        for (int x=0; x < 5;x++)
-        {
-            player.takeCard(dealer.dealACard());
-        }
-    */
-}
 }
 
 
